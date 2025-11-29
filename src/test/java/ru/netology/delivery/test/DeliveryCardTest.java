aa8db4b1d62a00146527fc64d8974a9183ad2bec
Irina Kyklina <dmitrieva.17.04@yandex.com>
2025-11-29 19:08:51 +0700

    Исправлены замечания преподавателя

--- src/test/java/ru/netology/delivery/test/DeliveryCardTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryCardTest.java
@@ -1,43 +1,61 @@
 package ru.netology.delivery.test;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import java.time.Duration;
+import org.openqa.selenium.Keys;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-class DeliveryCardTest {
+public class DeliveryCardTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        Configuration.timeout = 15000; // 15 секунд на ожидание элементов
+    }
+
     @BeforeEach
-    void setup() {
+    void setUp() {
         open("http://localhost:9999");
     }
 
     @Test
-    void shouldSuccessfullySubmitValidData() {
-        // Подготовка данных
+    @DisplayName("Should successfully deliver card with valid data")
+    void shouldSuccessfullyDeliverCard() {
+        // 1. Подготовка данных
+        String city = "Москва";
+        String name = "Иванов Иван";
+        String phone = "+79001234567";
         String date = LocalDate.now().plusDays(3).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        String expectedNotificationText = "Встреча успешно забронирована на " + date;
 
-        // Заполнение формы
-        $("[data-test-id='city'] input").setValue("Москва");
+        // 2. Заполнение формы
+        $("[data-test-id='city'] input").setValue(city);
 
-        SelenideElement dateInput = $("[data-test-id='date'] input");
-        dateInput.doubleClick();
-        dateInput.sendKeys(date);
+        // Очистка и ввод даты
+        $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(date);
 
-        $("[data-test-id='name'] input").setValue("Иванов Иван");
-        $("[data-test-id='phone'] input").setValue("+79001234567");
+        $("[data-test-id='name'] input").setValue(name);
+        $("[data-test-id='phone'] input").setValue(phone);
         $("[data-test-id='agreement']").click();
-        $(byText("Забронировать")).click();
 
-        // Проверки
+        // 3. Отправка формы
+        $$("button").find(Condition.text("Забронировать")).click();
+
+        // 4. Проверка результата
         $(".notification__content")
-                .shouldBe(visible, Duration.ofSeconds(15))
-                .shouldHave(text("Встреча успешно забронирована на"));
+                .shouldBe(Condition.visible)
+                .shouldHave(Condition.exactText(expectedNotificationText));
     }
 }
